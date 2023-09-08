bd40fc7f1eb1fbd2b4889cd043b076872a5b2687
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-09-08 14:03:38 +0530

    Working on login screen design

--- app/src/main/java/com/jecky/jetpackcoposefirebase/ui/login/LoginScreen.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/ui/login/LoginScreen.kt
@@ -1,25 +1,46 @@
 package com.jecky.jetpackcoposefirebase.ui.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jecky.jetpackcoposefirebase.R
 
 @Composable
 fun LoginScreen(loginClicked: () -> Unit) {
-    Column(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
-        
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.user_placeholder),
+            contentDescription = "User",
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp)
+        )
     }
 
 }
 
 @Preview
 @Composable
-fun PreviewLoginScreen(){
-    LoginScreen (loginClicked = {
+fun PreviewLoginScreen() {
+    LoginScreen(loginClicked = {
 
     })
 }
