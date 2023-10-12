3e1065e785489eb272727374d0e0672a35df94d7
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-10-12 15:47:10 +0530

    Fixed border color issue

--- app/src/main/java/com/jecky/jetpackcoposefirebase/util/EmailFieldState.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/util/EmailFieldState.kt
@@ -1,6 +1,6 @@
 package com.jecky.jetpackcoposefirebase.util
 
-open class EmailFieldState : TextFieldState(validator = ::isEmailValid, errorMessage = ::inValidEmailError)
+open class EmailFieldState : TextFieldState(validator = ::isEmailValid, errorMessage = ::inValidEmailError, changeFocus = ::hasFocus)
 
 fun isEmailValid(email:String): Boolean{
     return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
@@ -8,4 +8,8 @@ fun isEmailValid(email:String): Boolean{
 
 fun inValidEmailError(): String{
     return "Invalid email"
+}
+
+fun hasFocus(hasFocus: Boolean): Boolean{
+    return hasFocus
 }
