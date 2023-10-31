df8726b08c1c1e259729d3e404fbc7f60228452e
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-10-31 16:22:24 +0530

    Added API for getting quotes

--- app/src/main/java/com/jecky/jetpackcoposefirebase/repository/QuotesRepository.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/repository/QuotesRepository.kt
@@ -10,14 +10,29 @@ class QuotesRepository {
 
     var fireStore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    suspend fun addQuote(quote: Quote): APIResult<Boolean>{
-        return try{
+    suspend fun addQuote(quote: Quote): APIResult<Boolean> {
+        return try {
             val apiResponse = fireStore.collection(AppConstants.TABLE_QUOTE).add(quote).await()
             APIResult.APISuccess(data = true)
-        }
-        catch (exception: Exception){
+        } catch (exception: Exception) {
             APIResult.APIFailure(errorMessage = exception.message)
         }
     }
 
+
+    suspend fun getQuotes(): APIResult<List<Quote>> {
+        return try {
+            val quoteList = arrayListOf<Quote>()
+            val apiResult = fireStore.collection(AppConstants.TABLE_QUOTE).get().await()
+            for (document in apiResult.documents) {
+                val quote = document.toObject(Quote::class.java)
+                if (quote != null) {
+                    quoteList.add(quote)
+                }
+            }
+            APIResult.APISuccess(data = null)
+        } catch (exception: Exception) {
+            APIResult.APIFailure(errorMessage = exception.message)
+        }
+    }
 }
--- app/src/main/java/com/jecky/jetpackcoposefirebase/ui/home/HomeScreen.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/ui/home/HomeScreen.kt
@@ -1,35 +1,69 @@
 package com.jecky.jetpackcoposefirebase.ui.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.jecky.jetpackcoposefirebase.R
 
 @Composable
 fun HomeScreen() {
-    Column(
+    val list = listOf("1", "2", "3")
+    Card(
         modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.off_white))
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "Home Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+            .fillMaxWidth()
+            .padding(10.dp),
+
+        ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(list) { item ->
+                Text(
+                    text = "Home Screen",
+                    color = Color.Black,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp
+                )
+                Text(
+                    text = "Author",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Divider(
+                    modifier = Modifier.padding(horizontal = 14.dp),
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.08f)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
 }
