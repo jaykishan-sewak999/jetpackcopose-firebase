5260fe0060bf09ef49acec558b23cfd47965a70f
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2023-12-26 18:25:04 +0530

    Added navigation for add quote screen from Profile screen

--- app/src/main/java/com/jecky/jetpackcoposefirebase/Destinations.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/Destinations.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.jecky.jetpackcoposefirebase.Destinations.ADD_QUOTE_SCREEN
 import com.jecky.jetpackcoposefirebase.Destinations.CATEGORY_SCREEN
 import com.jecky.jetpackcoposefirebase.Destinations.HOME_SCREEN
 import com.jecky.jetpackcoposefirebase.Destinations.HOME_TAB_SCREEN
@@ -16,7 +17,9 @@ import com.jecky.jetpackcoposefirebase.ui.home.HomeScreen
 import com.jecky.jetpackcoposefirebase.ui.hometab.HomeTabScreen
 import com.jecky.jetpackcoposefirebase.ui.login.LoginRoute
 import com.jecky.jetpackcoposefirebase.ui.profile.ProfileScreen
+import com.jecky.jetpackcoposefirebase.ui.quote.AddQuoteScreen
 import com.jecky.jetpackcoposefirebase.ui.register.RegisterRoute
+import com.jecky.jetpackcoposefirebase.util.AppConstants.ADD_QUOTE_ID
 
 object Destinations {
     const val LOGIN_SCREEN = "login"
@@ -25,6 +28,8 @@ object Destinations {
     const val HOME_SCREEN = "home/{category}"
     const val PROFILE_SCREEN = "profile"
     const val CATEGORY_SCREEN = "category"
+    const val ADD_QUOTE_SCREEN = "add_quote"
+
 }
 
 @Composable
@@ -59,13 +64,20 @@ fun NavGraphDashboard(navController: NavHostController = rememberNavController()
             HomeScreen(category)
         }
         composable(PROFILE_SCREEN) {
-            ProfileScreen()
+            ProfileScreen(onItemClicked = {
+                when (it) {
+                    ADD_QUOTE_ID -> navController.navigate(ADD_QUOTE_SCREEN)
+                }
+            })
             //AddQuoteScreen()
         }
         composable(CATEGORY_SCREEN) {
             CategoryRoute(CategoryClicked = { categoryId ->
                 navController.navigate("home/$categoryId")
             })
+        }
+        composable(ADD_QUOTE_SCREEN) {
+            AddQuoteScreen()
         }
     }
 }
--- app/src/main/java/com/jecky/jetpackcoposefirebase/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/ui/profile/ProfileScreen.kt
@@ -3,6 +3,7 @@ package com.jecky.jetpackcoposefirebase.ui.profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,14 +13,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountBox
+import androidx.compose.material.icons.outlined.ArrowForward
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -30,9 +42,10 @@ import androidx.compose.ui.unit.sp
 import com.jecky.jetpackcoposefirebase.R
 import com.jecky.jetpackcoposefirebase.ui.theme.md_theme_light_onSecondary
 import com.jecky.jetpackcoposefirebase.ui.theme.purple_profile
+import com.jecky.jetpackcoposefirebase.util.AppConstants.ADD_QUOTE_ID
 
 @Composable
-fun ProfileScreen() {
+fun ProfileScreen(onItemClicked: (Int) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -51,10 +64,12 @@ fun ProfileScreen() {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(2.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(2.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Image(
                         painter = painterResource(id = R.drawable.ic_action_setting),
                         contentDescription = "logo",
@@ -98,13 +113,55 @@ fun ProfileScreen() {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(170.dp)
-                .padding(10.dp)
-                .clip(shape = RoundedCornerShape(10.dp))
-                .border(width = 1.dp, color = Color.Gray,shape = RoundedCornerShape(10.dp))
-                .background(color = md_theme_light_onSecondary), contentAlignment = Alignment.TopCenter
-        ) {
 
+                .padding(15.dp)
+                .clip(shape = RoundedCornerShape(10.dp))
+                .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(10.dp))
+                .background(color = md_theme_light_onSecondary),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+
+                ProfileButtonItem(
+                    icon = Icons.Outlined.Edit,
+                    title = "Edit Profile",
+                    onClick = {
+
+                    })
+                Spacer(modifier = Modifier.height(5.dp))
+                Divider()
+                Spacer(modifier = Modifier.height(5.dp))
+                ProfileButtonItem(
+                    icon = Icons.Outlined.List,
+                    title = "My Quote",
+                    onClick = {
+
+                    })
+                Spacer(modifier = Modifier.height(5.dp))
+                Divider()
+                Spacer(modifier = Modifier.height(5.dp))
+                ProfileButtonItem(
+                    icon = Icons.Outlined.AccountBox,
+                    title = "Add Quote",
+                    onClick = {
+                        onItemClicked(ADD_QUOTE_ID)
+                    })
+                Spacer(modifier = Modifier.height(5.dp))
+                Divider()
+                Spacer(modifier = Modifier.height(5.dp))
+                ProfileButtonItem(
+                    icon = Icons.Outlined.ExitToApp,
+                    title = "Logout",
+                    onClick = {
+
+                    })
+
+            }
         }
 
     }
@@ -113,5 +170,43 @@ fun ProfileScreen() {
 @Preview
 @Composable
 fun PreviewLoginScreen() {
-    ProfileScreen()
+    ProfileScreen(onItemClicked = {
+
+    })
+}
+
+@Composable
+fun ProfileButtonItem(icon: ImageVector, title: String, onClick: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().clickable {
+            onClick()
+        }
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = title,
+            tint = purple_profile,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                color = Color.Gray,
+                fontSize = 18.sp
+            )
+            Icon(
+                imageVector = Icons.Outlined.ArrowForward,
+                contentDescription = "Arrow",
+                tint = purple_profile,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+        }
+
+    }
 }
