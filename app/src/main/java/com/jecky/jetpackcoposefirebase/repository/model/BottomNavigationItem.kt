8c5d9ef266e45290bc639156c15adcb858d2fd4b
jaykishan-sewak <jaykishanbs@infusionanalysts.com>
2024-01-09 17:52:29 +0530

    Fixed no data found issue

--- app/src/main/java/com/jecky/jetpackcoposefirebase/repository/model/BottomNavigationItem.kt
+++ app/src/main/java/com/jecky/jetpackcoposefirebase/repository/model/BottomNavigationItem.kt
@@ -1,12 +1,11 @@
 package com.jecky.jetpackcoposefirebase.repository.model
 
 import com.jecky.jetpackcoposefirebase.Destinations.CATEGORY_SCREEN
-import com.jecky.jetpackcoposefirebase.Destinations.HOME_SCREEN
 import com.jecky.jetpackcoposefirebase.Destinations.PROFILE_SCREEN
 import com.jecky.jetpackcoposefirebase.R
 
 sealed class BottomNavigationItem(val title: String, val icon: Int, val route: String){
-    object Home: BottomNavigationItem(title = "Home", icon = R.drawable.ic_action_home_filled, route = HOME_SCREEN)
+    object Home: BottomNavigationItem(title = "Home", icon = R.drawable.ic_action_home_filled, route = "home/{category}/{fetchMyQuotes}")
     object Category: BottomNavigationItem(title = "Category", icon = R.drawable.ic_action_category_out_lined, route = CATEGORY_SCREEN)
     object Profile: BottomNavigationItem(title = "Profile", icon = R.drawable.ic_action_profile_outlined, route = PROFILE_SCREEN)
 }
